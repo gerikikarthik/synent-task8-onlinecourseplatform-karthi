7bc5a07ad5e5e84a002b724d40a3df3816b634e5
gerikikarthik@gmail.com <gerikikarthik@gmail.com>
2026-07-12 23:28:39 +0530

    Update Login API URL

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://synent-task8-onlinecourseplatform-karthi.onrender.com/api/auth/login",
         {
           email,
           password,
